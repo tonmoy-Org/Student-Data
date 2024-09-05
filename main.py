a492707b9266cf5d2a2eb0a62bf8c66db6cf44fe
tonmoy-Org <tonmoy15-5200@diu.edu.bd>
2024-09-05 23:19:15 +0600

    initial commit

--- main.py
+++ main.py
@@ -2,23 +2,24 @@ import pandas as pd
 
 df = pd.read_csv('genderClass.csv')
 
-print(df.head(2))
-print(df.tail(3))
-print(df.shape)
-print(df.columns)
+# print(df.head())
+# print(df.tail(3))
+# print(df.shape)
+# print(df.columns)
+#
+# print(df[['HairColor', 'Height']])
+# print(df.HairColor)
+#
+# print(df.size)
+# print(df.dtypes)
+#
+# print(df.values)
+# print(df.index)
 
-print(df[['HairColor', 'Height']])
-print(df.HairColor)
-
-print(df.size)
-print(df.dtypes)
-
-print(df.values)
-print(df.index)
-
-
-df['Age'] = df['Age'].fillna(df['Age'].mean())
-df['Height'] = df['Height'].fillna(df['Height'].mean())
+#
+# df['Age'] = df['Age'].fillna(df['Age'].mean())
+# print(df['Age'].isnull().sum())
+# df['Height'] = df['Height'].fillna(df['Height'].mean())
 
 
 # print(df.isnull().sum())
@@ -26,13 +27,21 @@ df['Height'] = df['Height'].fillna(df['Height'].mean())
 # Calculate the frequency of each hair color
 hairFrequency = df['HairColor'].value_counts()
 
+# print(hairFrequency)
+#  Apply one-hot encoding to the 'Color' column
+one_hot_encoded_df = pd.get_dummies(df['HairColor'], columns='HairColor')
+print(one_hot_encoded_df)
+
 # Find the most frequent hair color
-most_frequent_hair_color = hairFrequency.idxmax()
+# most_frequent_hair_color = hairFrequency.idxmax()
+# min_frequent_hair_color = hairFrequency.idxmin()
+# print(most_frequent_hair_color)
+# print(min_frequent_hair_color)
 
 # Fill missing values in the 'HairColor' column with the most frequent hair color
-df['HairColor'].fillna(most_frequent_hair_color, inplace=True)
-
-print(df.isnull().sum())
-
-
-print(df['HairColor'].head(15))
+# df['HairColor'].fillna(most_frequent_hair_color, inplace=True)
+#
+# print(df.isnull().sum())
+#
+#
+# print(df['HairColor'].head(5))
